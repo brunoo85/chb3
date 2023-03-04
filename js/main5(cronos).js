4ebda7b6bb7a475cbe9f97c12239458c71cf242a
Bruno Cesar <83827977+brunoo85@users.noreply.github.com>
2023-03-04 15:33:07 -0300

    fim pt.11
    AGORA DEU

--- js/main5(cronos).js
+++ js/main5(cronos).js
@@ -1,18 +1,18 @@
-let senhaDigitada2;
+let senhaDigitada5;
 var aparece = document.querySelector(".aparece4");
 
-function senha3 (){
-   senhaDigitada2 = prompt("Coloque a senha aqui");
+function senha4 (){
+   senhaDigitada5 = prompt("Coloque a senha aqui");
    
-   if(senhaDigitada2==="brunnmigi"){
+   if(senhaDigitada5==="brunnmigi"){
       aparece.style.display="flex";
    }else {
       alert("senha incorreta");
    }
   }
 
-  const botao = document.querySelector(".botao");
+  const botao = document.querySelector(".botao4");
 
   botao.onclick = function(){
-   senha3();
+   senha4();
 }
